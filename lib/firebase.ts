a7ba74ba77b00670b98cd88d712e4ac9b7b0b4bd
Arshad <68086718+Arshad9633@users.noreply.github.com>
2025-12-17 18:26:58 +0100

    Clear Firebase config values for security
    Removed sensitive Firebase configuration details.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMdaR-Xr9oRDnx1oWg6wPV780qHEZ5BrI",
-  authDomain: "bookingapp-4e6a4.firebaseapp.com",
-  projectId: "bookingapp-4e6a4",
-  storageBucket: "bookingapp-4e6a4.firebasestorage.app",
-  messagingSenderId: "502398321158",
-  appId: "1:502398321158:web:b07f54a08d02d5bf7f7987",
-  measurementId: "G-QJJB7M7TFN"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
